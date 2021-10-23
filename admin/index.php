9e7761b588f28ee24abae7a89c47b7729b62aec7
Stelios Papadopoulos <steliospapcs@gmail.com>
2021-10-23 17:39:33 +0300

    first commit

---
+++ admin/index.php
@@ -0,0 +1,32 @@
+<?php include "includes/admin_header.php" ?>
+
+<div id="wrapper">
+
+    <!-- Navigation -->
+
+
+    <div id="page-wrapper">
+        <?php include "includes/admin_navigation.php" ?>
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <h1 class="page-header">
+                        Welcome to admin's panel
+                        <small>Author</small>
+                    </h1>
+
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+
+    <?php include "includes/admin_footer.php" ?>
